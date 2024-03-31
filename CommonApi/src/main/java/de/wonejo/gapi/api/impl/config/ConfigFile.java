1a27004c0d2f745dd3e2d79f567ababe0919f4e0
MrBunnyDc <wonejo0109@gmail.com>
2024-03-31 19:12:37 -0400

    Add rewrite function in ConfigFile

--- CommonApi/src/main/java/de/wonejo/gapi/api/impl/config/ConfigFile.java
+++ CommonApi/src/main/java/de/wonejo/gapi/api/impl/config/ConfigFile.java
@@ -8,9 +8,12 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ConfigFile implements IConfigFile {
 
@@ -35,10 +38,12 @@ public class ConfigFile implements IConfigFile {
     public void init() {
         this.save();
 
+        String content = this.getFileContent();
+
         if (!this.configFile.exists()) {
             try {
                 LOGGER.warn("Creating configuration file: {}", this.fileName);
-                this.createFile();
+                this.createFile(content);
             } catch (IOException pException) {
                 LOGGER.warn("Fail creating {} configuration file", this.fileName);
                 LOGGER.trace(pException.toString());
@@ -46,11 +51,20 @@ public class ConfigFile implements IConfigFile {
             }
         }
 
-        this.provider.configurations().clear();
 
         if ( !this.isBroken() ) {
             try {
                 LOGGER.debug("Loading configurations from: {}", this.configFile);
+
+                Map<String, IConfigValue<?>> fileConfigurations = getFileConfigurations();
+                Set<String> providedConfigKeys = this.provider.configurations().keySet();
+
+                if (!fileConfigurations.keySet().containsAll(providedConfigKeys))
+                    rewrite();
+
+                this.provider.configurations().clear();
+                fileConfigurations.clear();
+
                 this.load();
             } catch (IOException pException) {
                 LOGGER.error("Error loading config in: {}", this.fileName);
@@ -63,7 +77,7 @@ public class ConfigFile implements IConfigFile {
     public void load() throws IOException {
         Scanner reader = new Scanner(this.configFile);
 
-        for ( int lineIndex = 1; reader.hasNextLine(); lineIndex++ ) {
+        while ( reader.hasNextLine() ) {
             String line = reader.nextLine();
 
             if (line.isEmpty() || line.startsWith("#")) continue;
@@ -83,18 +97,48 @@ public class ConfigFile implements IConfigFile {
         this.provider.configurations().clear();
     }
 
-    private Object parseConfigValue (String pValue) {
-        if ( pValue.equalsIgnoreCase("true") || pValue.equalsIgnoreCase("false") )
-            return Boolean.parseBoolean(pValue);
+    private void rewrite ( ) {
+        LOGGER.warn("Config file rewrite required. ( All actual configuration will stay there )");
 
-        try {
-            return Integer.parseInt(pValue);
-        } catch (NumberFormatException pEx) {
-            try {
-                return Double.parseDouble(pValue);
-            } catch (NumberFormatException pEx2 ) {
-                return pValue;
+        try (BufferedReader reader = Files.newBufferedReader(this.configFile.toPath());
+             BufferedWriter writer = Files.newBufferedWriter(this.configFile.toPath())) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("=")) {
+                    writer.write(line);
+                    writer.newLine();
+                } else {
+                    String comment = line.trim();
+                    String key = comment.substring(1, comment.indexOf("||") - 1).trim();
+                    IConfigValue<?> configValue = this.provider.configurations().get(key);
+
+                    writer.write(comment);
+                    writer.newLine();
+
+                    if (configValue != null) {
+                        String value = configValue.get().toString();
+                        writer.write(key + "=" + value);
+                        writer.newLine();
+                    }
+                }
             }
+
+            for (Map.Entry<String, IConfigValue<?>> entry : this.provider.configurations().entrySet()) {
+                String key = entry.getKey();
+                if (!isKeyPresent(key, this.configFile)) {
+                    String comment = "#" + entry.getValue().comment() + " || DEFAULT: " + entry.getValue().defaultValue();
+                    String value = entry.getValue().get().toString();
+                    writer.write(comment);
+                    writer.newLine();
+                    writer.write(key + "=" + value);
+                    writer.newLine();
+                }
+            }
+
+        } catch (IOException e) {
+            LOGGER.error("Error in rewrite of file");
+            throw new IllegalStateException(e);
         }
     }
 
@@ -103,11 +147,9 @@ public class ConfigFile implements IConfigFile {
             this.provider.buildConfigurations();
     }
 
-    private void createFile () throws IOException {
-        String configFile = this.getFileContent();
-
+    private void createFile (String pConfigContent) throws IOException {
         FileWriter writer = new FileWriter(this.configFile, StandardCharsets.UTF_8);
-        writer.write(configFile);
+        writer.write(pConfigContent);
         writer.close();
     }
 
@@ -124,6 +166,65 @@ public class ConfigFile implements IConfigFile {
         }
 
         return builder.toString();
+    }
+
+    private Map<String, IConfigValue<?>> getFileConfigurations () throws IOException {
+        Map<String, IConfigValue<?>> fileConfigurations = new HashMap<>();
+
+        try ( BufferedReader reader = Files.newBufferedReader(this.configFile.toPath()) ) {
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                line = line.trim();
+
+                if (!line.isEmpty()) {
+                    String comment = "";
+
+                    if (line.startsWith("#")) {
+                        comment = line;
+                    }
+
+                    String[] parts = line.split("=", 2);
+
+                    if (parts.length == 2) {
+                        String key = parts[0].trim();
+                        String value = parts[1].trim();
+
+                        Object configValue = parseConfigValue(value);
+                        fileConfigurations.put(key, new ConfigValue<>(key, comment, configValue));
+                    }
+                }
+            }
+        }
+
+        return fileConfigurations;
+    }
+
+    private boolean isKeyPresent(String key, File configFile) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(configFile.toPath())) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("#") && line.contains(key)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private Object parseConfigValue (String pValue) {
+        if ( pValue.equalsIgnoreCase("true") || pValue.equalsIgnoreCase("false") )
+            return Boolean.parseBoolean(pValue);
+
+        try {
+            return Integer.parseInt(pValue);
+        } catch (NumberFormatException pEx) {
+            try {
+                return Double.parseDouble(pValue);
+            } catch (NumberFormatException pEx2 ) {
+                return pValue;
+            }
+        }
     }
 
     public ConfigFile provider (IConfigProvider pProvider )  {
