daf446003294ca9099c78585d9017d4fe5bf0215
MrBunnyDc <wonejo0109@gmail.com>
2024-04-21 11:33:42 -0400

    Update ExampleBook

--- Fabric/src/main/java/de/wonejo/gapi/example/ExampleBook.java
+++ Fabric/src/main/java/de/wonejo/gapi/example/ExampleBook.java
@@ -41,7 +41,7 @@ public class ExampleBook implements IGuidebook {
                 .subHeader(Component.literal("-- Example SubHeader --"))
                 .itemName(Component.literal("Example Book"))
                 .author(Component.literal("WonejoMB"))
-                .bookColor(new Color(228, 18, 120))
+                .bookColor(new Color(140, 18, 32))
                 .contentProvider(this::contentProvider);
     }
 
