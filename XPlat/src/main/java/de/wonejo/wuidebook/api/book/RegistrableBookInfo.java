f5dfe2d639d16acb099145269cd2ea3962623a94
MrBunnyDc <wonejo0109@gmail.com>
2024-09-23 02:17:55 -0400

    Set more info in RegistrableBookInfo

--- XPlat/src/main/java/de/wonejo/wuidebook/api/book/RegistrableBookInfo.java
+++ XPlat/src/main/java/de/wonejo/wuidebook/api/book/RegistrableBookInfo.java
@@ -1,5 +1,6 @@
 package de.wonejo.wuidebook.api.book;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
 import java.awt.*;
@@ -7,7 +8,15 @@ import java.awt.*;
 public interface RegistrableBookInfo {
 
     ResourceLocation bookId ();
+
     ResourceLocation modelLocation ();
+    ResourceLocation mainBookGUITexture ();
+    ResourceLocation pageBookGUITexture ();
+
+    Component title ();
+    Component header ();
+    Component itemName ();
+    Component author ();
 
     Color bookColor ();
 
